c28578031c00a5a84138f3fa8e9124bd72586caa
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-07-02 12:24:54 +0100

    Initial Commit

---
+++ src/main/java/com/tsola2002/calculatorapp/CalculatorappApplication.java
@@ -0,0 +1,13 @@
+package com.tsola2002.calculatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsola2002/calculatorapp/CalculatorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsola2002.calculatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
